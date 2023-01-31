640762449dcc0d0abd718c80483ba4b36d6058ee
FabioBassani <avilar.fabio@yahoo.com.br>
2023-01-31 20:26:14 +0000

    Initial commit

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Danki Code - Tema</title>
+    <meta name="viewport" content="width=device-width,initial-scale=1.0,maximum-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <section class="topo">
+        <div class="center">
+            <header>
+                <div class="logo"><img src="images/logo.png"></div><!-- logo -->
+            </header>
+            <ul class="menu-desktop">
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Sobre</a></li>
+                <li><a href="#">Contato</a></li>
+            </ul>
+            <div class="clear"></div>
+            <br>
+            <br>
+            <div class="w50 time-descricao">
+                <h2>Melhore a comunicação com seu cliente e time.</h2>
+                <p>Consultoria especializada em startups, empresas, principalmente, pessoas.</p>
+                <a href="https://dankicode.com" target="_blank">Ver demonstração</a>
+            </div><!-- w50 -->
+
+            <div class="w50 time-imagem">
+                <img src="images/ilustracao.png" alt="">
+            </div><!-- w50 -->
+            <div class="clear"></div>
+
+        </div><!-- center -->
+    </section><!-- topo -->
+</body>
+
+</html>
